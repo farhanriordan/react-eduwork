afe9573f8c1114a99a6fa6d42686e2383fa3b631
farhanriordan <farhanriordan@gmail.com>
2022-11-22 22:14:03 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1 className="title">Learn Cats</h1>
+        <div className="color">
+          CATS
+          <div className="ungu">dwpodmweom</div>
+          <div className="bg"></div>
+          <div className="grid">
+            <div className="p1"></div>
+            <div className="p2"></div>
+            <div className="p3"></div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
